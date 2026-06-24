a4936039f4a28fa6e808a5b3cb1cc74e96b14d18
Srisaketh Peddi <138947478+speddi5288@users.noreply.github.com>
2026-06-24 15:23:02 -0400

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        mapS = {}
+        mapT = {}
+
+        for char in s:
+            mapS[char] = mapS.get(char, 0) + 1
+
+        
+        for char in t:
+            mapT[char] = mapT.get(char, 0) + 1
+
+        if mapT == mapS:
+            return True
+        
+        return False
+        
+
+            
+
+        
